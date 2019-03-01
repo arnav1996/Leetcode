e33b42c1d2ebd7d8d75505cb7f150dc78267f1ba
Arnav Shah <arnav1996@gmail.com>
2019-03-01 11:16:47 -0500

    Create 94. Binary Tree Inorder Traversal.py

---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,26 @@
+/*Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+*/
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        res, stack = [], []
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                root = node.right
+        return res
