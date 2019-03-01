a58cfcda809a19d3eef4056097a6e55cf1b89cb6
Arnav Shah <arnav1996@gmail.com>
2019-03-01 11:05:00 -0500

    Create 148. Sort List.py

---
+++ 148. Sort List.py
@@ -0,0 +1,25 @@
+/*Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+*/
+
+class Solution:
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        List = []
+        while head:
+            List.append(head.val)
+            head = head.next
+        return sorted(List)
